2f33deed0944ecf88e115fc8d55ff3317fd9af6d
VinhNguyenQuangHD <61536993+VinhNguyenQuangHD@users.noreply.github.com>
2022-03-01 13:28:45 +0700

    a them matkhau

--- KiemTra_T3_1_3_2022/KiemTra_T3_1_3_2022/Form1.Designer.cs
+++ KiemTra_T3_1_3_2022/KiemTra_T3_1_3_2022/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_matkhau = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(248, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "user name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(375, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 1;
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(331, 79);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(125, 27);
+            this.tb_username.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(248, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Mật khẩu";
+            // 
+            // tb_matkhau
+            // 
+            this.tb_matkhau.Location = new System.Drawing.Point(335, 128);
+            this.tb_matkhau.Name = "tb_matkhau";
+            this.tb_matkhau.Size = new System.Drawing.Size(125, 27);
+            this.tb_matkhau.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_matkhau);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox tb_username;
+        private Label label3;
+        private Label label4;
+        private TextBox tb_matkhau;
     }
 }
